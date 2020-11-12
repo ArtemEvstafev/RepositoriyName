3ee77540481d6cf021c0d767914a08b2c6e675c4
ArtemEvstafev <vur1artem1cheb@gmail.com>
2020-11-12 10:08:12 +0300

    Это функция меняет местами любые типы файлов.(С++)

---
+++ Swap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+template<typename T>
+T Swap(T &a, T &b)
+{
+    T tmp;
+    tmp=a;
+    a=b;
+    b=tmp;
+}
+int main()
+{
+
+    int a=10, b=20;
+    cout<<a<<" "<<b<<endl;
+    Swap(a, b);
+    cout<<a<<" "<<b<<endl;
+    return 0;
+}
